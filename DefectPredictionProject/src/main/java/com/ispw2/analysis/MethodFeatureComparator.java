8c913dd2a4551cee3b43eb2412ef6987a9d17733
leonardomonnati2796 <leonardo.monnati@gmail.com>
2026-01-07 14:53:20 +0100

    MethodFeatureComparator: log Path directly instead of calling toString()

--- DefectPredictionProject/src/main/java/com/ispw2/analysis/MethodFeatureComparator.java
+++ DefectPredictionProject/src/main/java/com/ispw2/analysis/MethodFeatureComparator.java
@@ -166,7 +166,7 @@ public class MethodFeatureComparator {
                     java.nio.file.StandardOpenOption.CREATE,
                     java.nio.file.StandardOpenOption.TRUNCATE_EXISTING);
             moveFileWithFallback(tmpFile, outFile);
-            log.info("Feature comparison CSV written to: {}", outFile.toString());
+            log.info("Feature comparison CSV written to: {}", outFile);
             return true;
         } catch (final IOException ioe) {
             if (attempt >= maxAttempts) {
